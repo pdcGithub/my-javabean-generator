20066ab8dd1dddc884a171aa8f63af465f4b025f
Dongcan Pang <pangdongcan@live.com>
2024-05-08 12:41:50 +0800

    main: project init.

---
+++ src/main/java/net/mickarea/generator/entrance/App.java
@@ -0,0 +1,30 @@
+/******************************************************************************************************
+
+This file "App.java" is part of project "generator" , which is belong to Michael Pang (It's Me).
+In my license, all codes can be shared free of charge. 
+However, if it is used for commercial purposes, I need to be notified.
+Here is my email "pangdongcan@live.com"
+
+Copyright (c) 2024 Michael Pang.
+
+*******************************************************************************************************/
+package net.mickarea.generator.entrance;
+
+/**
+ * &gt;&gt;&nbsp;[关于类中功能的描述，以英文句号结尾] 测试...TODO
+ * @author Michael Pang (Dongcan Pang)
+ * @version 1.0
+ * @since 2024年5月8日
+ */
+public class App {
+
+	/**
+	 * &gt;&gt;&nbsp;[关于方法中功能的描述，以英文句号结尾] 测试...
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("stdou...ok");
+	}
+
+}
